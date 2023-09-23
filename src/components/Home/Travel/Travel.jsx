54e6ae5b55b204e201f4c0eb6747690194bdfecf
Milot Qorrolli <milot.qorrolli@student.uni-pr.edu>
2023-09-23 18:25:27 +0200

    secondpart

--- src/components/Home/Travel/Travel.jsx
+++ src/components/Home/Travel/Travel.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import "./travel.scss"
-
+import "./travel.scss";
+import travel from "../../../assets/images/travel-desktop (1).webp";
+import { Link } from "react-router-dom";
 function Travel() {
   return (
     <div className="travel-container">
@@ -10,9 +11,9 @@ function Travel() {
             <h1>High Flyer</h1>
           </div>
           <div className="desc-wrapper">
-            <p>
-              <strong>Built for (spontaneous) travel.</strong>
-            </p>
+            <div className="titlep">
+              <p>Built for (spontaneous) travel.</p>
+            </div>
             <p>
               No matter where your next grand tour takes you, Atlas is there as
               your co-pilot, with bespoke flight benefits for when you're ready
@@ -20,8 +21,72 @@ function Travel() {
             </p>
           </div>
         </div>
-        <div className="image-wrapper"></div>
-        <div className="list-wrapper"></div>
+      </div>
+      <div className="image-wrapper">
+        <img src={travel} alt="travel" />
+      </div>
+      <div className="smaller-title">
+        <p>BESPOKE FLIGHT BENEFITS, INSIDE ATLAS APP.</p>
+      </div>
+      <div className="list-container">
+        <div className="list-wrapper">
+          <div className="list-center">
+            <div className="list-title">
+              <p>5x points with BLADE³</p>
+            </div>
+            <div className="list-desc">
+              <p>
+                Members get seated at prime time, when availability is typically
+                the hardest to get.Exclusive reward for Atlas cardholders on all
+                BLADE helicopter rides and flights.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="list-wrapper">
+          <div className="list-center">
+            <div className="list-title">
+              <p>CLEAR Plus membership⁴</p>
+            </div>
+            <div className="list-desc">
+              <p>
+                Fast forward through airport security with included CLEAR Plus
+                annual membership.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="list-wrapper">
+          <div className="list-center">
+            <div className="list-title">
+              <p>Cancel flights without fees⁵</p>
+            </div>
+            <div className="list-desc">
+              <p>
+                Change or cancel any flight, at any time, for any reason and get
+                fees reimbursed up to $600 a year.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="last-container">
+        <div className="last-wrapper">
+          <div className="last-title">
+            <p>+</p>
+            <p>Visa Infinite Benefits⁶</p>
+          </div>
+          <div className="last-desc">
+            <p>
+              Priority Pass, Luxury Hotel Collection, no foreign transaction
+              fees, and more.
+            </p>
+          </div>
+
+          <Link to="/visa">
+            <button>Discover</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/pages.jsx/Home.jsx
+++ src/pages.jsx/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home1 from '../components/Home/Home1/Home1'
 import Dining from '../components/Home/Dining/Dining'
 import Travel from '../components/Home/Travel/Travel'
+import Card from '../components/Home/Card/Card'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Home1 />
       <Dining />
       <Travel />
+      <Card />
     </div>
   )
 }
